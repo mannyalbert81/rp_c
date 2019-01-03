7822140bb23baac1ba95c86dffec6cc325b4ac8a
Usuario <Usuario@DESKTOP-NM3SASK>
2019-01-03 11:37:37 -0500

    MAYCOL FLORES Permisos Roles

--- view/modulos/links_js.php
+++ view/modulos/links_js.php
@@ -5,3 +5,21 @@
     <script src="view/bootstrap/bower_components/fastclick/lib/fastclick.js"></script>
     <script src="view/bootstrap/dist/js/adminlte.min.js"></script>
     <script src="view/bootstrap/dist/js/demo.js"></script>
+    
+    
+    <!-- PARA DATA TABLES -->
+    <script src="view/bootstrap/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="view/bootstrap/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    <script>
+      $(function () {
+        $('#example1').DataTable()
+        $('#example2').DataTable({
+          'paging'      : true,
+          'lengthChange': false,
+          'searching'   : false,
+          'ordering'    : true,
+          'info'        : true,
+          'autoWidth'   : false
+        })
+      })
+	</script>

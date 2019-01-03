867e2bb02c12e1444a94ecf5fdd99a42299ad51b
Manuel <Manuel@192.168.1.123>
2019-01-03 14:32:39 -0500

    Roles boton remove

--- view/RolesView.php
+++ view/RolesView.php
@@ -53,8 +53,7 @@
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
                   <i class="fa fa-minus"></i></button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-                  <i class="fa fa-times"></i></button>
+                
               </div>
             </div>
             
@@ -106,9 +105,7 @@
               <h3 class="box-title">Listado de Roles Registrados</h3>
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                  <i class="fa fa-minus"></i></button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-                  <i class="fa fa-times"></i></button>
+                  <i class="fa fa-minus"></i></button>                
               </div>
             </div>
         

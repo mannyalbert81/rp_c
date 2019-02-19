1ee07e415e69b7145e725033f718f04396ab7201
Manuel <Manuel@DANNY-DESARROLLO>
2019-02-19 14:23:07 -0500

    sweetalert closeOnClickOutside:false

--- view/bootstrap/otros/inventario/movimientos_salidas_detalle.js
+++ view/bootstrap/otros/inventario/movimientos_salidas_detalle.js
@@ -44,3 +44,83 @@ function aprobar_producto(id){
     	}
 	});
 }
+
+$('.enviarsalida').on('click',function(){
+	$('#btnForm').val($(this).val())
+	//console.log($(this).val())
+})
+
+$('#frm_agrega_salida').on('submit',function(event){
+	
+	var parametros = $(this).serialize();	
+	
+	//console.log('accion=ajax&'+parametros)
+	
+	var btnform = document.getElementById('btnForm').value
+	
+	$.ajax({
+        beforeSend: function(objeto){
+          
+        },
+        url: 'index.php?controller=MovimientosInv&action=inserta_salida',
+        type: 'POST',
+        data: 'accion=ajax&'+parametros,
+        dataType:'json',
+        success: function(respuesta){
+        	
+        	//console.log(respuesta)
+        	
+        	//$("#frm_guardar_producto")[0].reset();
+        	var valrespuesta = respuesta.mensaje.includes("ok")||false
+        	
+            if(valrespuesta){
+            	
+            	swal({
+            		  title: "Salidas",
+            		  text: respuesta.mensaje,
+            		  icon: "success",
+            		  buttons: {Aceptar: {text: "Aceptar",value: "ok" }}
+            		})
+            		.then((value) => {
+        			  switch (value) {            			  
+        			    case "ok":
+        			    	window.location="index.php?controller=MovimientosInv&action=indexsalida"
+        			      break;            			 
+        			  }
+            		});
+				
+                }else{
+                	
+                	swal({
+              		  title: "Salidas",
+              		  text: respuesta.mensaje,
+              		  icon: "warning",
+              		  button: "Aceptar",
+              		  closeOnClickOutside: false,
+              		});
+                }
+        	
+        	
+        		//setTimeout("redireccionarPagina()", 5000);
+        
+        	     
+        },
+        error: function(xhr,estado,error){
+        	//console.log(xhr.responseText);
+			 var err=xhr.responseText
+			 
+			 swal({
+        		  title: "Error",
+        		  text: "Error conectar con el Servidor \n "+err,
+        		  icon: "error",
+        		  button: "Aceptar",
+        		});
+        }
+    });
+	
+	event.preventDefault();	
+})
+
+function redireccionarPagina() {
+	window.location="index.php?controller=MovimientosInv&action=indexsalida"
+}

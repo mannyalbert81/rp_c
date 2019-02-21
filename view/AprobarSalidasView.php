afc0562568ee12f7bd094b006ccbfce0b6eee54c
Maycol <Maycol@STEVEN-PC.CAPREMCI.INT>
2019-02-21 16:10:31 -0500

    jlkjlk

--- view/AprobarSalidasView.php
+++ view/AprobarSalidasView.php
@@ -51,7 +51,7 @@
           <section class="content">
           <div class="box box-primary">
             <div class="box-header with-border">
-              <h3 class="box-title">Aprobar/Rechazar Solicitudes dsasdasdasd </h3>
+              <h3 class="box-title">Aprobar/Rechazar Solicitudes </h3>
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
                   <i class="fa fa-minus"></i></button>

17b5f3e71d996614ad64b0c7c91ce7742d5a4025
programador1 <programador1@DANNY-DESARROLLO.CAPREMCI.INT>
2019-06-21 10:02:43 -0500

    reportes

--- view/reportes/CuentasPagarRpt.php
+++ view/reportes/CuentasPagarRpt.php
@@ -7,6 +7,14 @@ include dirname(__FILE__).'\..\..\view\mpdf\mpdf.php';
 $header = file_get_contents('view/reportes/template/CabeceraFinal.html');
 $template = file_get_contents('view/reportes/template/CuentasPagar.html');
 
+if(!empty($datos_cabecera))
+{
+    
+    foreach ($datos_cabecera as $clave=>$valor) {
+        $template = str_replace('{'.$clave.'}', $valor, $template);
+    }
+}
+
 if(!empty($datos_empresa))
 {
     
@@ -19,13 +27,7 @@ if(!empty($datos_empresa))
 $footer = file_get_contents('view/reportes/template/pieret.html');
 
 
-if(!empty($datos_cabecera))
-{
-    
-    foreach ($datos_cabecera as $clave=>$valor) {
-        $template = str_replace('{'.$clave.'}', $valor, $template);
-    }
-}
+
 
 if(!empty($datos_cuentas_pagar))
 {
@@ -40,15 +42,16 @@ if(!empty($datos_cuentas_pagar))
 ob_end_clean();
 //creacion del pdf
 //$mpdf=new mPDF('c','A4','','' , 0 , 0 , 0 , 0 , 0 , 0);
-$mpdf=new mPDF();
+$mpdf= new mPDF('utf-8','A4');
 $mpdf->SetDisplayMode('fullpage');
 $mpdf->allow_charset_conversion = true;
-$mpdf->charset_in = 'UTF-8';
 $mpdf->setAutoTopMargin = 'stretch';
 $mpdf->setAutoBottomMargin = 'stretch';
 $mpdf->SetHTMLHeader(utf8_encode($header));
 $mpdf->SetHTMLFooter($footer);
-$mpdf->WriteHTML($template);
+$stylesheet = file_get_contents('view/reportes/template/template.css'); // la ruta a tu css
+$mpdf->WriteHTML($stylesheet,1);
+$mpdf->WriteHTML($template,2);
 $mpdf->debug = true;
 $mpdf->Output();
 /*$content = $mpdf->Output('', 'S'); // Saving pdf to attach to email

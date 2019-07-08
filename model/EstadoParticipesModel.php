08cd02f51bdae567680684d663dc063f01af73ca
Cristian Nolivos <Cristian Nolivos@DESKTOP-KTLTNR7.CAPREMCI.INT>
2019-07-08 08:22:49 -0500

    fffff

--- model/EstadoParticipesModel.php
+++ model/EstadoParticipesModel.php
@@ -1,6 +1,6 @@
 <?php
 class EstadoParticipesModel extends ModeloBase{
-<<<<<<< HEAD
+
 	private $table;
 	private $where;
 	private $funcion;
@@ -83,63 +83,3 @@ class EstadoParticipesModel extends ModeloBase{
     
 }
 ?>
-=======
-	
-	private $table;
-	private $where;
-	private $funcion;
-	private $parametros;
-	
-	public function getWhere() {
-		return $this->where;
-	}
-	
-	public function setWhere($where) {
-		$this->where = $where;
-	}
-	
-	public function getFuncion() {
-		return $this->funcion;
-	}
-	
-	
-	public function setFuncion($funcion) {
-		$this->funcion = $funcion;
-	}
-	
-	
-	
-	public function getParametros() {
-		return $this->parametros;
-	}
-	
-	
-	public function setParametros($parametros) {
-		$this->parametros = $parametros;
-	}
-	
-	
-	
-	
-	public function __construct(){
-		$this->table="core_estado_participes";
-		
-		parent::__construct($this->table);
-	}
-	
-
-	public function Insert(){
-		
-		$query = "SELECT ".$this->funcion."(".$this->parametros.")";
-		
-		$resultado=$this->enviarFuncion($query);
-			
-			
-		return  $resultado;
-	}
-	
-	
-	
-}
-?>
->>>>>>> branch 'master' of https://github.com/mannyalbert81/rp_c.git

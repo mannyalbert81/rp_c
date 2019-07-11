61c5a135585202252c7474ab107e2f9e6771e89a
programador1 <programador1@DANNY-DESARROLLO.CAPREMCI.INT>
2019-07-11 15:46:19 -0500

    solicitud view

--- view/Inventario/SolicitudView.php
+++ view/Inventario/SolicitudView.php
@@ -37,6 +37,7 @@
          </aside>
 
   	  <div class="content-wrapper">
+  	  <form id="frm_solicitud_cabeza" action="<?php echo $helper->url("MovimientosInv","inserta_solicitud"); ?>" method="post" enctype="multipart/form-data"  class="col-lg-12 col-md-12 col-xs-12">
         <section class="content-header">
           <h1>
             <small><?php echo $fecha; ?></small>
@@ -52,7 +53,7 @@
           <section class="content">
           <div class="box box-primary">
             <div class="box-header with-border">
-              <h3 class="box-title">Estado Solicitud</h3>
+              <h3 class="box-title">Generacion Solicitud</h3>
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
                   <i class="fa fa-minus"></i></button>
@@ -60,341 +61,104 @@
               </div>
             </div>
             
-            <div class="box-body">
-            	<div class="ibox-content">
-                      	
-                  <div class="x_content">
-                  
-                  <div class="col-lg-6 col-md-6 col-xs-12">
-                      <div class="box box-default box-solid">
-                        <div class="box-header with-border">
-                          <h3 class="box-title">Productos Disponibles</h3>
-            
-                          <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                          </div>
-                          <!-- /.box-tools -->
-                        </div>
-                        <!-- /.box-header -->
-                        <div class="box-body">
-                          <div class="pull-right" style="margin-right:11px;">
-        					<input type="text" value="" class="form-control" id="buscador_productos" name="buscador_productos" onkeyup="load_productos_solicitud(1)" placeholder="search.."/>
-        				</div>
-        				<div id="load_productos" ></div>	
-        				<div id="productos_inventario"></div>
-                        </div>
-                        <!-- /.box-body -->
-                      </div>
-                      <!-- /.box -->
-                   </div>
-                  
+             <div class="box-body">
+             
+             	<input type="hidden" name="id_usuario" id="id_usuario" value="<?php if( isset($resultSet) ) { echo $resultSet[0]->id_usuarios; } ?>" />  
+             
+             	<div id="panelmodal">
+          			<div class="row">
+          				<div class="col-lg-6 col-md-6 col-xs-12 form-group-sm ">
+                            <label for="razon_solicitud" class="col-sm-3 form-label"> Observacion:</label>
+                            <div class="col-sm-9">
+                              <textarea class="md-textarea form-control" rows="1" id="razon_solicitud" name="razon_solicitud" ></textarea>
+                              <div id="mensaje_razon_solicitud" class="errores"></div>
+                            </div>
+                            
+                         </div>
+                         <div class="col-lg-6 col-md-6 col-xs-12">
+                             <div class="pull-right">          					     
+              					<div class="form-group-sm">
+              					           					
+              						 <button type="submit" id="Guardar" name="Guardar" class="btn btn-default">
+              						 <i aria-hidden="true" class="fa fa-floppy-o"></i> Guardar Solicitud</button>                  				
+                    				 <button type="button" id="btnAgregarProductos" name="btnAgregarProductos" class="btn btn-default" data-toggle="modal" data-target="#mod_listado_productos">
+                    				 <i aria-hidden="true" class="fa fa-plus"></i> Agregar Producto a Solicitud</button>
+                        		</div>            			
+                      		</div>
+                        </div>          				      				
+          				
+          			 </div> 					
+          		</div>
+          		
+                
+                <hr> 	
+                
+                <div>
+                <div id="resultados" ></div>
+                </div>	
+             	                  
                  
-    				
-                    <div class="col-lg-6 col-md-6 col-xs-12">
-                      <div class="box box-default box-solid">
-                        <div class="box-header with-border">
-                          <h3 class="box-title">Productos Solicitados</h3>
-            
-                          <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                          </div>
-                          <!-- /.box-tools -->
-                        </div>
-                        <!-- /.box-header -->
-                        <div class="box-body">
-                          <div id="resultados" ></div>
-                        </div>
-                        <!-- /.box-body -->
-            
-                                 	
-                      		
-                  			</div>
-                  	</div>
+		     </div>
+	     	</div>
+    		     
     		</section>
     		
-    	        <section class="content">
-              <div class="box box-success">
-                <div class="box-header with-border">
-                  <h3 class="box-title"></h3>
-                  <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                      <i class="fa fa-minus"></i></button>
-                    
-                  </div>
-                </div>
-                
-                <div class="box-body">
-                <form id="frm_solicitud_cabeza" action="<?php echo $helper->url("MovimientosInv","inserta_solicitud"); ?>" method="post" enctype="multipart/form-data"  class="col-lg-12 col-md-12 col-xs-12">
-                 <?php if(!empty($resultSet)){ foreach ($resultSet as $res){?>                		    
-                      	  <div class="row">
-                		  	<div class="col-xs-10 col-md-6 col-lg-6 ">
-                            	<div class="form-group">
-                                    <input type="hidden" name="id_usuario" id="id_usuario" value="<?php echo $res->id_usuarios; ?>" class="form-control"/>
-    					            <div id="mensaje_nombre_grupos" class="errores"></div>
-                                 </div>
-                             </div>
-                             
-                          </div>
-                         
-                    		            
-                     <?php }}?>
-            	 	<div class="row">
-                      	<div class="col-xs-12 col-md-12 col-lg-12 ">
-                        	<!--  <div class="form-group">-->
-                        		<div class="md-form">
-  									<i class="fas fa-pencil-alt prefix"></i>
-                            		<label for="razon_solicitud">Observacion:</label>
-  									<textarea class="md-textarea form-control" rows="1" id="razon_solicitud" name="razon_solicitud" ></textarea>
-                                	<div id="mensaje_razon_solicitud" class="errores"></div>
-                                </div>
-                             <!-- </div> -->
-                         </div>
-                     </div> 
-                    <div class="row">
-        			    <div class="col-xs-12 col-md-2 col-md-2 " >
-            	   		    <div class="form-group">
-            	   		    	<label for="Guardar">&nbsp;</label>
-        	                  <button type="submit" id="Guardar" name="Guardar" class="form-control btn btn-success">Guardar</button>
-    	                    </div>
-	        		    </div>
-        		    </div>
-                </form>
-                </div>
-                </div>
-            </section>
-            
+            </form>
   		</div>
   
   <?php include("view/modulos/footer.php"); ?>	
 
    <div class="control-sidebar-bg"></div>
  </div>
+ 
+ <!-- PARA MODALES --> 
+ <div class="modal fade" id="mod_listado_productos" data-backdrop="static" data-keyboard="false">
+      <div class="modal-dialog" style="width:70%">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">Listado Productos</h4>
+          </div>
+          <div class="modal-body">
+          <!-- empieza el formulario modal productos -->
+          	<form class="" method="post" id="frm_listado_productos" name="frm_listado_productos">
+          	
+          		<div class="row">
+      				<div class=" pull-left " >
+      					<div class=" col-lg-12 form-group-sm">
+                        	<span class="form-control" id="mod_cantidad_busqueda"><strong>Registros: </strong>0</span>
+                        	<input type="hidden" value="" id="total_query" name="total_query"/>
+                    	</div>   
+                	</div>
+      				<div class="pull-right">
+      					<div class="col-lg-12 form-group-sm">                    				
+            				 <input type="text" class="form-control" id="mod_txtbuscar" name="mod_txtbuscar" value="" >
+                		</div>            			
+              		</div>
+          		</div>  
+          		<br/>
+          		<div id="div_tabla_productos">
+  				</div>
+             	<div class="clearfix"></div>		  
+          	</form>
+          	<!-- termina el formulario modal de impuestos -->
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+          </div>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+</div>
+    
     
     
    <?php include("view/modulos/links_js.php"); ?>
+   <script src="view/bootstrap/otros/notificaciones/notify.js"></script>
+   <script type="text/javascript" src="view/Inventario/js/Solicitud.js?0.06"></script>
     	
   </body>
 </html>
-
-<!-- script pagina anterior -->
-<script type="text/javascript">
-     
-        	   $(document).ready( function (){
-        		   //pone_espera();
-        		   
-	   			});
-
-        	   function pone_espera(){
-
-        		   $.blockUI({ 
-        				message: '<h4><img src="view/images/load.gif" /> Espere por favor, estamos procesando su requerimiento...</h4>',
-        				css: { 
-        		            border: 'none', 
-        		            padding: '15px', 
-        		            backgroundColor: '#000', 
-        		            '-webkit-border-radius': '10px', 
-        		            '-moz-border-radius': '10px', 
-        		            opacity: .5, 
-        		            color: '#fff',
-        		           
-        	        		}
-        	    });
-            	
-		        setTimeout($.unblockUI, 3000); 
-		        
-        	   }
-
- </script>
- 
- <script type="text/javascript">
- 	$(document).ready(function(){
- 		load_productos_solicitud(1);
- 		load_temp_solicitud(1);
- 		
- 	});
-
- 	function load_productos_solicitud(pagina){
-
- 	   var search=$("#buscador_productos").val();
-        var con_datos={
- 				  action:'ajax',
- 				  page:pagina,
- 				  buscador:search
- 				  };
-      
-      $.ajax({
-                beforeSend: function(objeto){
-                  $("#load_productos").fadeIn('slow');
-                  $("#load_productos").html('<center><img src="view/images/ajax-loader.gif"> Cargando...</center>');
-                },
-                url: 'index.php?controller=SolicitudCabeza&action=ajax_trae_productos',
-                type: 'POST',
-                data: con_datos,
-                success: function(x){
-                  $("#productos_inventario").html(x);
-                  $("#load_productos").html("");
-                  $("#tabla_productos").tablesorter(); 
-                  
-                },
-               error: function(jqXHR,estado,error){
-                 $("#productos_inventario").html("Ocurrio un error al cargar la informacion de Usuarios..."+estado+"    "+error);
-               }
-             });
-
-
- 	   }
-
- 	
- 	function agregar_producto (id)
-	{
-		var cantidad=document.getElementById('cantidad_'+id).value;
-		//Inicia validacion
-		if (isNaN(cantidad))
-		{
-		alert('Esto no es un numero');
-		document.getElementById('cantidad_'+id).focus();
-		return false;
-		}
-		
-		$.ajax({
-            type: "POST",
-            url: 'index.php?controller=SolicitudCabeza&action=insertar_producto',
-            data: "id_productos="+id+"&cantidad="+cantidad,
-        	 beforeSend: function(objeto){
-        		/*$("#resultados").html("Mensaje: Cargando...");*/
-        	  },
-            success: function(datos){
-        		$("#resultados").html(datos);
-        	}
-		});
-	}
-
- 	function eliminar_producto (id)
-	{
-		
-		$.ajax({
-            type: "POST",
-            url: 'index.php?controller=SolicitudCabeza&action=eliminar_producto',
-            data: "id_solicitud="+id,
-        	 beforeSend: function(objeto){
-        		$("#resultados").html("Mensaje: Cargando...");
-        	  },
-            success: function(datos){
-        		$("#resultados").html(datos);
-        	}
-		});
-	}
-
- 	function load_temp_solicitud(pagina){
-  	  
-         var con_datos={
-  				  page:pagina
-  				  };
-       
-       $.ajax({
-                 beforeSend: function(objeto){
-                   
-                 },
-                 url: 'index.php?controller=SolicitudCabeza&action=trae_temporal',
-                 type: 'POST',
-                 data: con_datos,
-                 success: function(x){
-                   $("#resultados").html(x);
-                   $("#tabla_temporal").tablesorter(); 
-                   
-                 },
-                error: function(jqXHR,estado,error){
-                  $("#resultados").html("Ocurrio un error al cargar la informacion de Usuarios..."+estado+"    "+error);
-                }
-              });
-
-
-  	   }
-
-</script>
-
-<script type="text/javascript">
-
-$(document).ready( function (){
-
-	$('#frm_solicitud_cabeza').submit(function( event ) {
-
-		if(!document.getElementById("total_query_temp_solicitud")){
-			swal({
-		   		  title: "Solicitud",
-		   		  text: "Agregue un registro a la solicitud",
-		   		  icon: "error",
-		   		  button: "Aceptar",
-		   		  closeOnClickOutside:false,
-		   		})
-			return false
-			
-			}
-
-		if(document.getElementById('razon_solicitud').value == '')
-		{
-			$('#mensaje_razon_solicitud').text('Ingrese una razon')
-			$('#mensaje_razon_solicitud').fadeIn('slow')
-			return false
-		}
-
-		var parametros = $(this).serialize();
-
-		
-       	 $.ajax({
-       		 beforeSend:function(){},
-       		 url:'index.php?controller=MovimientosInv&action=inserta_solicitud',
-       		 type:'POST',
-       		 data:parametros+'&peticion=ajax',
-       		 dataType: 'json',
-       		 success: function(respuesta){
-           		console.log(respuesta.mensaje);
-       			 if(respuesta.status==1){
-       				 $("#frm_solicitud_cabeza")[0].reset();
-    	            		swal({
-       	            		  title: "Solicitud",
-       	            		  text: respuesta.mensaje,
-       	            		  icon: "success",
-       	            		  button: "Aceptar",
-       	            		});
-       					
-       	                }else{
-       	                	$("#frm_solicitud_cabeza")[0].reset();
-       	                	swal({
-       	              		  title: "Solicitud",
-       	              		  text: respuesta.mensaje,
-       	              		  icon: "warning",
-       	              		  button: "Aceptar",
-       	              		});
-       	             }
-       			load_temp_solicitud(1)
-       			
-       		 },
-       		 error: function(jqXHR,estado,error){
-       	        
-       	        }
-       	 })
-
-		event.preventDefault(); 
-		})
-
-	 
-	   
-	});
-
-$('#razon_solicitud').on('focus',function(){
-	$('#mensaje_razon_solicitud').fadeOut('slow')
-})
-
-
-</script>
-       
-       
-      
- 
-
-
-
 

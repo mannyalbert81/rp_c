c184e5449589b37f9f037fa3c2e4408094154ee0
programador1 <programador1@DANNY-DESARROLLO.CAPREMCI.INT>
2019-09-06 16:56:58 -0500

    recaudaciones

--- view/Recaudaciones/ArchivoEntidadPatronalView.php
+++ view/Recaudaciones/ArchivoEntidadPatronalView.php
@@ -20,6 +20,11 @@
         background: url('view/images/ajax-loader.gif') 50% 50% no-repeat rgb(249,249,249);
         opacity: .8;
         }
+        
+      .modal-body {
+            max-height: calc(100vh - 210px);
+            overflow-y: auto;
+        }
  	  
  	</style>
    <?php include("view/modulos/links_css.php"); ?>
@@ -132,7 +137,8 @@
                 				<label for="formato_recaudacion" class="col-sm-4 control-label" >Formato:</label>
                 				<div class="col-sm-8">
                                   	<select id="formato_recaudacion" name="formato_recaudacion" class="form-control">
-                                  	<option value="1" >ENVIO DESCUENTOS APORTE</option>
+                                  	<option value="1" >DESCUENTOS APORTES</option>
+                                  	<option value="2" >DESCUENTOS CREDITOS</option>
                                   	</select>
                                  </div>
                 			 </div>        			 
@@ -165,10 +171,10 @@
                                   	<!-- <button type="button" id="btnDistribuir" name="btnDistribuir" class="btn btn-block btn-sm btn-default">DISTRIBUIR</button> -->
                                  </div>
                                  <div class="col-sm-4">
-                                  	<button type="button" id="btnSimular" name="btnSimular" class="btn btn-block btn-sm btn-default">SIMULAR</button>
+                                  	<button type="button" id="btnGenerar" name="btnGenerar" class="btn btn-block btn-sm btn-default">GENERAR</button>
                                  </div>
                                  <div class="col-sm-4">
-                                  	<button type="button" id="btnGenerar" name="btnGenerar" class="btn btn-block btn-sm btn-default">GENERAR</button>
+                                  	<button type="button" id="btnDescargar" name="btnDescargar" class="btn btn-block btn-sm btn-default">DESCARGAR ARCHIVO</button>
                                  </div>
                 			 </div>        			 
             			</div>
@@ -202,11 +208,11 @@
             <br>
               <div class="tab-pane active" id="his_datos">
                 
-					<div class="pull-right" style="margin-right:15px;">
-					<input type="text" value="" class="form-control" id="txtBuscar" name="txtBuscar"  placeholder="Buscar.."/>
+					<div class="pull-right" >
+					<input type="text" value="" class="form-control" id="txtBuscarDatos" name="txtBuscarDatos"  placeholder="Buscar.."/>
         			</div>           
         			<div class="clearfix" ></div> 	
-                	<div id="div_tabla_archivo" ></div>
+                	<div id="datos_recaudacion" ></div>
                 
               </div>
               
@@ -245,10 +251,42 @@
    <div class="control-sidebar-bg"></div>
  </div>
  
+ <!-- Para modales -->
+  <div class="modal fade" id="mod_datos_archivo" data-backdrop="static" data-keyboard="false">
+      <div class="modal-dialog   modal-lg " role="document" >
+        <div class="modal-content">
+          <div class="modal-header bg-aqua disabled color-palette">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" align="center">DATOS ARCHIVO</h4>
+          </div>
+          <div class="modal-body" >
+          	<div class="box-body">
+          		<div class=" pull-left " >
+  					<div class="form-group-sm">
+                    	<span class="form-control" id="cantidad_busqueda"><strong>Total Registros: </strong> <span id="mod_cantidad_registros"></span> </span>
+                	</div>   
+            	</div>
+          		<div class="pull-right" style="margin-right:15px;">
+    			<input type="text" value="" class="form-control" onkeyup="buscarDatos()" id="mod_txtBuscarDatos" name="mod_txtBuscarDatos"  placeholder="Buscar.."/>
+    			</div>           
+    			<div class="clearfix" ></div> 	
+            	<div id="mod_div_datos_recaudacion"></div>
+          	</div>
+          	
+          
+          </div>
+          
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+</div>
+ 
  <div class="modal fade" id="mod_recaudacion" data-backdrop="static" data-keyboard="false">
       <div class="modal-dialog" style="width:40%">
         <div class="modal-content">
-          <div class="modal-header">
+          <div class="modal-header bg-orange disabled color-palette">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span></button>
             <h4 class="modal-title">Cambio Valores Aporte</h4>
@@ -258,41 +296,65 @@
           	<form class="form " method="post" id="frm_edit_recaudacion" name="frm_edit_recaudacion">
           	
           	<div class="row">
-          		<div class="col-lg-12 col-md-12 col-xs-12">        		
+          		<div class="col-lg-12 col-md-12 col-xs-12">
+          			<h5>Datos Participe</h5>
+          			<input type="hidden" class="form-control " id="mod_id_archivo_detalle" name="mod_id_archivo_detalle" >
+          		</div>
+              	<div class="col-lg-12 col-md-12 col-xs-12">        		
         			<div class="form-group "> 
             			 <div class="form-group-sm">            			 	
-            				<p class="text-muted col-sm-4 control-label">Metodo Descuento:</p>
+            				<p class="text-muted col-sm-4 control-label">Cedula:</p>
             				<div class="col-sm-8">
-                              	<input type="text" class="form-control " id="mod_metodo_descuento" name="mod_metodo_descuento" readonly>
-                              	<input type="hidden" class="form-control " id="mod_id_archivo" name="mod_id_archivo" >
+                              	<input type="text" class="form-control " id="mod_cedula_participes" name="mod_cedula_participes" readonly>
                              </div>
             			 </div>        			 
         			</div>
 				</div>
 				<div class="col-lg-12 col-md-12 col-xs-12">        		
         			<div class="form-group "> 
-            			 <div class="form-group-sm">
-            				<p class="text-muted col-sm-4 control-label">Valor Descuento Sistema:</p>
+            			 <div class="form-group-sm">            			 	
+            				<p class="text-muted col-sm-4 control-label">Nombres:</p>
             				<div class="col-sm-8">
-                              	<input type="text" class="form-control " id="mod_valor_sistema" name="mod_valor_sistema" readonly>
+                              	<input type="text" class="form-control " id="mod_nombres_participes" name="mod_nombres_participes" readonly>                              	
                              </div>
             			 </div>        			 
         			</div>
 				</div>
 				<div class="col-lg-12 col-md-12 col-xs-12">        		
         			<div class="form-group "> 
+            			 <div class="form-group-sm">            			 	
+            				<p class="text-muted col-sm-4 control-label">Apellidos:</p>
+            				<div class="col-sm-8">
+                              	<input type="text" class="form-control " id="mod_apellidos_participes" name="mod_apellidos_participes" readonly>
+                             </div>
+            			 </div>        			 
+        			</div>
+				</div>
+              	<div class="col-lg-12 col-md-12 col-xs-12">        		
+    			<div class="form-group "> 
+        			 <div class="form-group-sm">
+        				<p class="text-muted col-sm-4 control-label">Valor Sistema:</p>
+        				<div class="col-sm-8">
+                          	<input type="text" class="form-control " id="mod_valor_sistema" name="mod_valor_sistema" readonly>
+                         </div>
+        			 </div>        			 
+    			</div>
+    			</div>
+    			<div class="col-lg-12 col-md-12 col-xs-12">        		
+        			<div class="form-group "> 
             			 <div class="form-group-sm">
-            				<p class="text-muted col-sm-4 control-label">Valor Descuento:</p>
+            				<p class="text-muted col-sm-4 control-label">Nuevo Valor:</p>
             				<div class="col-sm-8">
                               	<input type="text" class="form-control " id="mod_valor_edit" name="mod_valor_edit" >
                              </div>
             			 </div>        			 
         			</div>
-				</div>
-          		
+    			</div>
           	</div>
           	
-          	<div id="msg_frm_recaudacion" ></div>          	
+          	<div id="msg_frm_recaudacion" ></div> 
+          	
+          	<div class="clearfix"></div>         	
 			  
           	</form>
           	<!-- termina el formulario modal de impuestos -->
@@ -315,7 +377,7 @@
    <script src="view/bootstrap/plugins/input-mask/jquery.inputmask.extensions.js"></script>
    <script src="view/bootstrap/otros/notificaciones/notify.js"></script>
    <script src="view/bootstrap/bower_components/select2/dist/js/select2.full.min.js"></script> 
-   <script src="view/Recaudaciones/js/archivoEntidadPatronal.js?0.10"></script> 
+   <script src="view/Recaudaciones/js/archivoEntidadPatronal.js?0.19"></script> 
        
        
 

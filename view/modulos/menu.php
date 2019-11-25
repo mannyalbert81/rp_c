ab1196bcea7a481ed807d8fcd7b8aad773c43c00
programador2 <programador2@DESKTOP-4JB888C.CAPREMCI.INT>
2019-11-25 09:55:57 -0500

    asda

--- view/modulos/menu.php
+++ view/modulos/menu.php
@@ -147,7 +147,7 @@ $controladores=$_SESSION['controladores'];
             </span>
           </a>
           <ul class="treeview-menu">
-            <li style="<?php echo getcontrolador("BuscarProducto",$controladores) ?>"><a href="index.php?controller=BuscarProducto&action=index"><i class="fa fa-circle-o"></i> Consultar Productos</a></li>
+            <li style="<?php echo getcontrolador("BuscarProducto",$controladores) ?>"><a href="index.php?controller=BuscarProducto&action=index"><i class="fa fa-circle-o"></i> Inventario de Productos</a></li>
 		 	</ul>
         </li>
        </ul>

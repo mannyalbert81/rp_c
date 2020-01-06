e32843de80b62fa4fde62657f0d76c2d367366ba
Cristian Nolivos <Cristian Nolivos@DESKTOP-KTLTNR7.CAPREMCI.INT>
2020-01-06 11:08:30 -0500

    ewr

--- view/Contable/FuncionesJS/Presupuestos.js
+++ view/Contable/FuncionesJS/Presupuestos.js
@@ -3,3 +3,116 @@ $(document).ready(function(){
 
 });
 
+function autompleteCodigo(elemento){
+	  
+	  var _elemento = $(elemento);
+	  //console.log("ingreso codigo complete");	  
+	  if ( !_elemento.data("autocomplete") ) {
+		  
+		  _elemento.autocomplete({
+	    		minLength: 3,    	    
+	    		source:function (request, response) {
+	    			$.ajax({
+	    				url:"index.php?controller=Presupuestos&action=autompleteCodigo",
+	    				dataType:"json",
+	    				type:"GET",
+	    				data:{term:request.term},
+	    			}).done(function(x){
+	    				
+	    				response(x); 
+	    				
+	    			}).fail(function(xhr,status,error){
+	    				var err = xhr.responseText
+	    				console.log(err)
+	    			})
+	    		},
+                 select: function (event, ui) {
+	    			
+	    			if(ui.item.id == ''){
+	    				 _elemento.notify("Digite Cod. Cuenta Valido",{ position:"buttom left"});
+	    				 $('#id_plan_cuentas').val(0);
+	    				 return;
+	    			}
+	    			
+	    			$('#id_plan_cuentas').val(ui.item.id);
+	    				    			     	     
+	     	    },
+	     	   appendTo: null,
+	     	   change: function(event,ui){
+	     		   if(ui.item == null){
+	     			   
+	     			 _elemento.notify("Digite Cod. Cuenta Valido",{ position:"buttom left"});
+	     			 _elemento.val("")
+	     			 $('#id_plan_cuentas').val(0);
+	 	   			 $('#nombre_presupuestos').val("");
+	 	   			 $('#valor_procesar').val("");
+	 	   			 $('#valor_ejecutar').val("");
+	    			 			 	   			
+	     			
+	     		   }
+	     	   }
+	    	
+	    	}).focusout(function() {
+	    		
+	    	})
+	  }
+	  
+}
+
+function InsertarPresupuestos(){
+	
+	var _id_plan_cuentas = document.getElementById('id_plan_cuentas').value;
+	var _nombre_presupuestos = document.getElementById('nombre_presupuestos').value;
+	var _valor_procesado = document.getElementById('valor_procesado').value;
+	var _valor_ejecutado = document.getElementById('valor_ejecutado').value;
+	
+	
+	
+	var parametros = {
+			id_plan_cuentas:_id_plan_cuentas,
+			nombre_presupuestos:_nombre_presupuestos,
+			valor_procesado:_valor_procesado,
+			valor_ejecutado:_valor_ejecutado
+	}
+	
+$.ajax({
+		
+		beforeSend:function(){},
+		url:"index.php?controller=Presupuestos&action=InsertarPresupuestos",
+		type:"POST",
+		dataType:"json",
+		data:parametros
+	}).done(function(datos){
+		
+		if(datos.mensaje != undefined && datos.mensaje == 1){
+			
+			swal({
+		  		  title: "Presupuestos",
+		  		  text: "Guardado exitosamente",
+		  		  icon: "success",
+		  		  button: "Aceptar",
+		  		
+		  		});
+			
+		}
+			console.log(datos)
+	
+		
+	}).fail(function(xhr,status,error){
+		var err = xhr.responseText
+		console.log(err);
+		
+	});
+	
+
+event.preventDefault()
+
+}
+
+
+
+
+
+
+	
+
--- view/Contable/PresupuestoView.php
+++ view/Contable/PresupuestoView.php
@@ -71,50 +71,47 @@
         <form id="frm_presupuestos" action="<?php echo $helper->url("Presupuestos","Index"); ?>" method="post" enctype="multipart/form-data"  class="col-lg-12 col-md-12 col-xs-12">
         
             <div class="row">
+            
+            
 			
-			<div class="col-xs-6 col-md-3 col-md-3 ">
+			<div class="col-xs-3 col-md-3 col-md-3 ">
     		    <div class="form-group">
                   <label for="nombre" class="control-label">Nombre</label>
-                  <input type="text" class="form-control" id="nombre" name="nombre" 
-                  value=""  placeholder="Nombre ">
-                  <input type="hidden" name="" id="" value="" class="form-control"/>
+                  <input type="text" class="form-control" id="nombre_presupuestos" name="nombre"  placeholder="Nombre ">
                 </div>
 		    </div>
-		    <div class="col-xs-6 col-md-3 col-md-3 ">
+		    <div class="col-xs-3 col-md-3 col-md-3 ">
     		    <div class="form-group">
                   <label for="valor_procesado" class="control-label">Valor procesado</label>
-                  <input type="number" class="form-control" id="valor_procesado" name="valor_procesado" 
-                  value=""  placeholder="valor_procesado" onKeyPress="return numeros(event)">
+                  <input type="text" class="form-control" id="valor_procesado" name="valor_procesado"  placeholder="Valor Procesado">
                 </div>
 		    </div>
-		</div>
 		
-		    <div class="row">
 		    		    
-		    <div class="col-xs-6 col-md-3 col-md-3 ">
+		    <div class="col-xs-3 col-md-3 col-md-3 ">
     		    <div class="form-group">
                   <label for="valor_ejecutado" class="control-label">Valor ejecutado</label>
-                  <input type="number" class="form-control" id="valor_ejecutado" name="valor_ejecutado" 
-                  value=""  placeholder="valor_ejecutado">
+                  <input type="text" class="form-control" id="valor_ejecutado" name="valor_ejecutado"  placeholder="Valor Ejecutado">
                 </div>
-		    </div>		    
-    		<div class="col-xs-6 col-md-3 col-lg-3">
-                  <div class="form-group">
-                  <label for="id_plan_cuentas" class="control-label">Estado:</label>
-                  <select name="plan_cuentas" id="plan_cuentas"  class="form-control" >
-                  <option value="0" selected="selected">--Seleccione--</option>
-    									
-  				 </select> 
-                 <div id="mensaje_plan_cuentas" class="errores"></div>
-                 </div>
-            </div>
+		    </div>
+		    
+		    <div class="col-xs-3 col-md-3 col-md-3 ">
+    		    <div class="form-group">
+                  <label for="codigo_plan_cuentas" class="control-label">Cod / Nom</label>
+                  <input type="text" class="form-control" id="codigo_plan_cuentas" name="codigo_plan_cuentas" onkeyup="autompleteCodigo(this)" value="" placeholder="Id Plan Cuentas">
+                  <div id="mensaje_id_plan_cuentas" class="errores"></div>
+                  <input type="hidden" name="id_plan_cuentas" id="id_plan_cuentas" value="0" class="form-control"/>
+                </div>
+		    </div>	
+		    
+    		
 		</div>
          
 		   <br>  
     	    <div class="row">
            	 <div class="col-xs-12 col-md-12 col-md-12 " style="margin-top:15px;  text-align: center; ">
             	<div class="form-group">
-                  <button type="button" id="Guardar" name="Guardar" class="btn btn-success" onclick="">GUARDAR</button>
+                  <button type="button" id="Guardar" name="Guardar" class="btn btn-success" onclick="InsertarPresupuestos()">GUARDAR</button>
                   <button type="button" class="btn btn-danger" id="Cancelar" name="Cancelar" onclick="">CANCELAR</button>
                 </div>
              </div>	    
@@ -138,14 +135,10 @@
 					<input type="text" value="" class="form-control" id="search" name="search" onkeyup="load_empleados(1)" placeholder="Buscar.."/>
 			</div>
 			<div class="pull-right" style="margin-right:15px;">
-        			<select name="" id="estado_empleados"  class="form-control" onchange="load_empleados(1)">
-                  		<?php  foreach($resultEst as $res) {?>
-						<option value="<?php echo $res->id_estado; ?>"><?php echo $res->nombre_estado; ?> </option>
-			        	<?php } ?>
-    				</select>
+        			
     		</div>
-        	<div id="load_empleados" ></div>
-        	<div id="empleados_registrados" ></div>
+        	<div id="load_presupuestos" ></div>
+        	<div id="presupuestos_registrados" ></div>
         </div> 	
   	</div>
   </section>
@@ -166,6 +159,6 @@
 	<script src="view/bootstrap/bower_components/jquery-ui-1.12.1/jquery-ui.js"></script> 
     <script src="view/bootstrap/otros/inputmask_bundle/jquery.inputmask.bundle.js"></script>
     <script src="view/bootstrap/otros/notificaciones/notify.js"></script>
-	<script src="view/Contable/FuncionesJS/Presupuestos.js?0.00"></script>
+	<script src="view/Contable/FuncionesJS/Presupuestos.js?0.08"></script>
   </body>
 </html>   

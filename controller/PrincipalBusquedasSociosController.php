147200a400bbcc7a3f3c5bfe1403d3da3e7b5844
Sistemas <Sistemas@DESKTOP-HQ9E3AA>
2020-04-13 17:12:24 -0500

    ingreso de transaccion

--- controller/PrincipalBusquedasSociosController.php
+++ controller/PrincipalBusquedasSociosController.php
@@ -121,6 +121,50 @@ class PrincipalBusquedasSociosController extends ControladorBase{
 	    echo json_encode($resp);
 	}
 	
+	/***
+	 * @desc function para dibujar bancos locales de la entidad por medio de request ajax
+	 * @param none
+	 * @author dc 2020/04/13
+	 */
+	public function CargaBancosLocal(){
+	    
+	    $pagos = new PagosModel();
+	    $resp  = null;
+	    
+	    $col1  = " id_bancos, nombre_bancos, lpad(index_bancos_chequera::text,espacio_bancos_chequera,'0') consecutivo_cheque , abrev_bancos_chequera";
+	    $tab1  = " tes_bancos ";
+	    $whe1  = " local_bancos = 't'";
+	    $id1   = " nombre_bancos ";
+	    $rsConsulta1   = $pagos->getCondiciones($col1, $tab1, $whe1, $id1);
+	    
+	    try {
+	        
+	        $error_pg = pg_last_error();
+	        if( !empty($error_pg) ){
+	            throw new Exception( $error_pg );
+	        }
+	        
+	        if( !empty($rsConsulta1) ){
+	            $resp['data'] = $rsConsulta1;
+	        }else{
+	            $resp['data'] = null;
+	        }
+	        
+	    } catch (Exception $e) {
+	        $buffer =  error_get_last();
+	        $resp['icon'] = isset($resp['icon']) ? $resp['icon'] : "error";
+	        $resp['mensaje'] = $e->getMessage();
+	        $resp['msgServer'] = $buffer; //buscar guardar buffer y guaradr en variable
+	        $resp['estatus'] = "ERROR";
+	    }
+	    
+	    error_clear_last();
+	    if (ob_get_contents()) ob_end_clean();
+	    
+	    echo json_encode($resp);
+	    
+	}
+	
 	
 	public function IngresaRegistroAporte(){
 	     
@@ -130,7 +174,7 @@ class PrincipalBusquedasSociosController extends ControladorBase{
 	    try {
 	        
 	        $id_participes             = $_POST['id_participes'];
-	        $imagen_registro           = $_FILES['imagen_registro'];
+	        $file_imagen_registro      = $_FILES['imagen_registro'];
 	        $numero_documento_registro = $_POST['numero_documento_registro'];
 	        $id_tipo_aportacion        = $_POST['id_tipo_aportacion'];
 	        $id_contribucion_tipo      = $_POST['id_contribucion_tipo'];
@@ -143,25 +187,81 @@ class PrincipalBusquedasSociosController extends ControladorBase{
 	        $id_tipo_transaccion       = $_POST['id_tipo_transaccion'];
 	        $id_tipo_ingresos_contribucion     = $_POST['id_tipo_ingresos_contribucion'];
 	        $id_bancos_registro        = $_POST['id_bancos_registro'];
+	        $id_entidad_patronal       = $_POST['id_entidad_patronal'];
+	        
+	        //tomar variables de la session
+	        if( !isset($_SESSION)){
+	            session_start();
+	        }
+	        
+	        $id_usuarios       = $_SESSION['id_usuarios'];
+	        $usuario_usuarios  = $_SESSION['usuario_usuarios'];
 	        
 	        if( !empty( error_get_last() ) ){ throw new Exception("Variable no recibida"); }
-	        var_dump($_FILES);
-	        var_dump($_POST); die();
 	        
-	        $col1  = " aa.id_participes ,aa.cedula_participes ,aa.nombre_participes ,aa.apellido_participes ,bb.id_entidad_patronal ,bb.nombre_entidad_patronal";
-	        $tab1  = " core_participes aa
-	        INNER JOIN core_entidad_patronal bb ON bb.id_entidad_patronal = aa.id_entidad_patronal ";
-	        $whe1  = " aa.id_participes = $id_participes ";
-	        $rsConsulta1   = $busquedas->getCondicionesSinOrden($col1, $tab1, $whe1, "");
+	        $imagen_registro   = 'null';
+	        if( $file_imagen_registro['tmp_name'] != "" ){
+	            $directorio = $_SERVER['DOCUMENT_ROOT'].'/rp_c/fotografias_documentos/';
+	            
+	            $nombre    = $file_imagen_registro['name'];
+	            //$tipo      = $_FILES['imagen_registro']['type'];
+	            //$tamano    = $_FILES['imagen_registro']['size'];
+	            
+	            move_uploaded_file($file_imagen_registro['tmp_name'],$directorio.$nombre);
+	            $data = file_get_contents($directorio.$nombre);
+	            $imagen_registro = pg_escape_bytea($data);
+	        }else{
+	            
+	            $directorio = dirname(__FILE__).'\..\view\images\usuario.jpg';	            
+	            $imagen_registro   = is_file( $directorio ) ? pg_escape_bytea( file_get_contents( $directorio ) ) : "null";	            
+	        }        
 	        
-	        $resp['dataParticipe'] = ( empty($rsConsulta1) ) ? null : $rsConsulta1;
+	        $participes->beginTran();
 	        
+	        /** valores seteados **/
+	        $id_estatus    = 1;
+	        $valor_patronal_contribucion   = 0.00;
+	        $id_ccomprobantes  = 'null';   //esta variable se llenara para realizar actualizado de la tabla
+	        $id_estado_contribucion    = 1;
+	        $descripcion_contribucion_registro = "REGISTO MANUAL";
+	        $id_liquidacion    = 0;
+	        $id_distribucion   = 0;
+	        $tipo_descuento_contribucion   = 1; 
 	        
+	        $funcion    = "core_ins_contribucion";
 	        
+	        $parametros = "$id_participes,'$fecha_transaccion_registro',$valor_aporte_registro,$valor_patronal_contribucion,$id_estatus,'$descripcion_contribucion_registro',
+            $id_ccomprobantes,$id_usuarios,'$usuario_usuarios',$id_contribucion_tipo,$id_estado_contribucion,$id_entidad_patronal,'$fecha_contable_registro',
+            '$numero_documento_registro','$observacion_registro',$id_liquidacion,$id_distribucion,$tipo_descuento_contribucion";
+	        
+            $StringQuery    = $participes->getconsultaPG($funcion, $parametros);
+            
+            $resultado  = $participes->llamarconsultaPG($StringQuery);
+            
+            $id_contribucion    = $resultado[0];
+            
+            if( !empty( pg_last_error() )){ throw  new Exception("ERROR EN EL INSERTADO DE CONTIBUCION"); }
+            
+                        
+            /** valores seteados para contribucion bancos **/
+            $id_estatus_contribucion    = 1;
+            $fnContribucionBancos   = "core_ins_contribucion_bancos";            
+            $paramContribucionBancos    = "$id_contribucion,$id_tipo_ingresos_contribucion,'$imagen_registro',$id_estatus_contribucion";            
+            $sqContribucionBancos    = $participes->getconsultaPG($fnContribucionBancos, $paramContribucionBancos);
+            
+            $resultado  = $participes->llamarconsultaPG($sqContribucionBancos);
+            
+            $id_contribucion_bancos    = $resultado[0];
+            
+            if( !empty( pg_last_error() )){ throw  new Exception("ERROR EN EL INSERTADO DE CONTIBUCION-BANCOS"); }
+	        
+            
 	        $error_pg = pg_last_error();
 	        if( !empty($error_pg) ){
 	            throw new Exception( $error_pg );
 	        }
+	        
+	        $participes->endTran("COMMIT");
 	        
 	    } catch (Exception $e) {
 	        $buffer =  error_get_last();
@@ -169,6 +269,7 @@ class PrincipalBusquedasSociosController extends ControladorBase{
 	        $resp['mensaje'] = $e->getMessage();
 	        $resp['msgServer'] = $buffer; //buscar guardar buffer y guaradr en variable
 	        $resp['estatus'] = "ERROR";
+	        $participes->endTran();
 	    }
 	    
 	    error_clear_last();
--- view/principal/js/vtnRegistroAportes.js
+++ view/principal/js/vtnRegistroAportes.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 	
 	console.log("AUI ESATMOS EN LA SUB VENTANA");
 	initControles();
+	loadBancosLocales();
 	loadDataParticipes();
 })
 
@@ -173,49 +174,152 @@ function fnValidaTipoIngreso(){
 	
 }
 
+/***
+ * @desc funcion para traer datos bancos locales
+ * @param none
+ * @retuns void
+ * @ajax si 
+ */
+function loadBancosLocales(){
+	
+	var ddlBancosContribucion	= $("#ddl_banco_registro");	 
+	
+    var params	= {};
+	
+	$.ajax({
+		url:"index.php?controller=PrincipalBusquedasSocios&action=CargaBancosLocal",
+		dataType:"json",
+		type:"POST",
+		data: params
+	}).done( function(x){
+		
+		if( x.data != undefined && x.data != "" ){
+			
+			var rsBancos	= x.data;
+			
+			ddlBancosContribucion.empty();
+			ddlBancosContribucion.append('<option value="0">--Seleccione--</option>' );
+			$.each(rsBancos,function(index, value){
+				//console.log('index -->'+index+'   Value ---> '+value.id_bancos);
+				ddlBancosContribucion.append( '<option value="'+value.id_bancos+'">'+value.nombre_bancos+'</option>' );
+			});
+					
+		}
+				
+	}).fail( function(xhr,status,error){
+		console.log(xhr.responseText);
+		ddlBancosContribucion.empty();
+		ddlBancosContribucion.append('<option value="0">--Seleccione--</option>' );
+	})
+	
+}
+
 function fnIngresarRegistro(){
 	
 	/** validacion de controles **/
+	//validacion del participe
 	var inid_participes	= $("#hdnid_participes");
 	if( inid_participes.val() == 0 || inid_participes.val() == "" ){
-		inid_participes.notify("Participe No Identificado",{ position:"buttom left", autoHideDelay: 2000});
+		swal({title:"ERROR",text:"Datos Partcipe No validos",icon:"warning"});
 		return false;
 	}
-	
+	//validacion campo imagen obligatorio 
 	var inimagen_registro = $("#imagen_registro");
 	if( inimagen_registro[0].files.length == 0){
-		inimagen_registro.notify("Ingrese un Imagen",{ position:"buttom left", autoHideDelay: 2000});
+		inimagen_registro.closest("tr").notify("Ingrese un Imagen",{ position:"buttom left", autoHideDelay: 2000});
 		return false;
 	}
-	
+	//validacion de numero documento 
 	var innumero_documento = $("#numero_documento_registro");
 	if( innumero_documento.val().length == 0 || innumero_documento.val() == "" ){
-		innumero_documento.notify("Ingrese un Imagen",{ position:"buttom left", autoHideDelay: 2000});
+		innumero_documento.closest("tr").notify("Ingrese numero documento",{ position:"buttom left", autoHideDelay: 2000});
 		return false;
 	}
-		
+	//validacion de tipo aporte registro
+	var intipo_aporte = $("#ddl_tipo_aporte_registro");
+	if( intipo_aporte.val() == 0 ){
+		intipo_aporte.closest("tr").notify("Seleccione tipo aporte",{ position:"buttom left", autoHideDelay: 2000});
+		return false;
+	}
+	//validacion datos descuento
+	var incontribucion_tipo = $("#hdn_id_tipo_aportacion");
+	if( incontribucion_tipo.val() == 0 ){
+		incontribucion_tipo.closest("tr").notify("Datos Descuentos No definidos",{ position:"buttom left", autoHideDelay: 2000});
+		return false;
+	}
+	//validacion ultimo sueldo
+	var inultimo_sueldo = $("#ultimo_sueldo_registro");
+	if( inultimo_sueldo.val().length == 0 || inultimo_sueldo.val() == "" ){
+		inultimo_sueldo.closest("tr").notify("Ingrese datos de ultimo sueldo",{ position:"buttom left", autoHideDelay: 2000});
+		return false;
+	}
+	//validacion valor aportar
+	var invalor_calculado 	= $("#valor_calculado_registro");
+	var invalor_aportar 	= $("#valor_aportar_registro");
+	if( invalor_aportar.val().length == 0 || invalor_aportar.val() == "" ){
+		invalor_aportar.closest("tr").notify("Valor aportar no definido",{ position:"buttom left", autoHideDelay: 2000});
+		return false;
+	}
+	//validacion observacion
+	var inobservacion_registro 	= $("#observacion_registro");
+	/*if( inobservacion_registro.val().length == 0 || inobservacion_registro.val() == "" ){
+		inobservacion_registro.closest("tr").notify("Valor aportar no definido",{ position:"buttom left", autoHideDelay: 2000});
+		return false;
+	}*/
+	//validacion fecha contable
+	var infecha_contable	= $("#fecha_contable_registro");
+	if( infecha_contable.val().length == 0 || infecha_contable.val() == "" ){
+		infecha_contable.closest("tr").notify("Fecha contable no definida",{ position:"buttom left", autoHideDelay: 2000});
+		return false;
+	}
+	//validacion fecha transaccion
+	var infecha_transaccion	= $("#fecha_transaccion_registro");
+	if( infecha_transaccion.val().length == 0 || infecha_transaccion.val() == "" ){
+		infecha_transaccion.closest("tr").notify("Fecha transaccion no definida",{ position:"buttom left", autoHideDelay: 2000});
+		return false;
+	}
+	//validacion de tipo aporte registro
+	var intipo_transaccion = $("#ddl_tipo_transaccion");
+	/*if( intipo_transaccion.val() == 0 ){
+		intipo_transaccion.closest("tr").notify("Seleccione tipo transaccion",{ position:"buttom left", autoHideDelay: 2000});
+		return false;
+	}*/
+	//validacion de tipo aporte registro
+	var intipo_ingreso = $("#ddl_tipo_ingreso");
+	if( intipo_ingreso.val() == 0 ){
+		intipo_ingreso.closest("tr").notify("Seleccione tipo ingreso",{ position:"buttom left", autoHideDelay: 2000});
+		return false;
+	}
+	//validacion de tipo aporte registro
+	var inbanco_registro = $("#ddl_banco_registro");
+	if( intipo_ingreso.val() != 2 ){
+		//validacion de banco registro
+		if( inbanco_registro.val() == 0 ){
+			inbanco_registro.closest("tr").notify("Seleccione banco registro",{ position:"buttom left", autoHideDelay: 2000});
+			return false;
+		}
+	}
+	/**datos de formulario cabeza**/	
+	inid_entidad_patronal = $("#id_entidad_patronal");
    
     var params = new FormData();
 	
     params.append('id_participes',inid_participes.val());
     params.append('imagen_registro',inimagen_registro[0].files[0]);
-    params.append('numero_documento_registro','ajax');
-    params.append('id_tipo_aportacion','ajax');
-    params.append('id_contribucion_tipo','ajax');
-    params.append('ultimo_sueldo_registro','ajax');
-    params.append('valor_calculado_registro','ajax');
-    params.append('valor_aporte_registro','ajax');
-    params.append('observacion_registro','ajax');
-    params.append('fecha_contable_registro','ajax');
-    params.append('fecha_transaccion_registro','ajax');
-    params.append('id_tipo_transaccion','ajax');
-    params.append('id_tipo_ingresos_contribucion','ajax');
-    params.append('id_bancos_registro','ajax');
-    
-    params.forEach((value,key) => {
-        console.log(key+" "+value)
-  	});
-	
+    params.append('numero_documento_registro',innumero_documento.val());
+    params.append('id_tipo_aportacion',intipo_aporte.val());
+    params.append('id_contribucion_tipo',incontribucion_tipo.val());
+    params.append('ultimo_sueldo_registro',inultimo_sueldo.val());
+    params.append('valor_calculado_registro',invalor_calculado.val());
+    params.append('valor_aporte_registro',invalor_aportar.val());
+    params.append('observacion_registro',inobservacion_registro.val());
+    params.append('fecha_contable_registro',infecha_contable.val());
+    params.append('fecha_transaccion_registro',infecha_transaccion.val());
+    params.append('id_tipo_transaccion',intipo_transaccion.val());
+    params.append('id_tipo_ingresos_contribucion',intipo_ingreso.val());
+    params.append('id_bancos_registro',inbanco_registro.val()); 
+    params.append('id_entidad_patronal',inid_entidad_patronal.val());
+        
 	$.ajax({
 		url:"index.php?controller=PrincipalBusquedasSocios&action=IngresaRegistroAporte",
 		dataType:"json",
@@ -228,14 +332,6 @@ function fnIngresarRegistro(){
 		console.log(x);
 		if( x.dataParticipe != undefined && x.dataParticipe != null ){
 			var rsParticipe	= x.dataParticipe[0];
-			
-			$("#lblIdentificacion").val( rsParticipe.cedula_participes );
-			$("#lblNombres").val( rsParticipe.nombre_participes );
-			$("#lblApellidos").val( rsParticipe.apellido_participes );
-			
-			$("#id_entidad_patronal").empty();
-			$("#id_entidad_patronal").append("<option value=\"0\"  >--Seleccione--</option>");
-			$("#id_entidad_patronal").append("<option value=\""+rsParticipe.id_entidad_patronal+"\" selected >"+rsParticipe.nombre_entidad_patronal+"</option>");
 					
 		}
 				

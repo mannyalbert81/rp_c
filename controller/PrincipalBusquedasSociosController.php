9bb53c8fbe8d7b4c8bba38ca0a172233b69cbeab
programador1 <programador1@DANNY-DESARROLLO.CAPREMCI.INT>
2020-04-16 11:37:07 -0500

    cambios

--- controller/PrincipalBusquedasSociosController.php
+++ controller/PrincipalBusquedasSociosController.php
@@ -140,29 +140,25 @@ class PrincipalBusquedasSociosController extends ControladorBase{
 	    try {
 	        
 	        $id_participes = $_POST['id_participes'];
+	        $anio_registro = $_POST['anio_registro'];
+	        $mes_registro  = $_POST['mes_registro'];
 	        
 	        if( !empty( error_get_last() ) ){ throw new Exception("Variable no recibida"); }
 	        
-	        $col1  = " aa.id_participes ,aa.cedula_participes ,aa.nombre_participes ,aa.apellido_participes ,bb.id_entidad_patronal ,bb.nombre_entidad_patronal";
-	        $tab1  = " core_participes aa
-	        INNER JOIN core_entidad_patronal bb ON bb.id_entidad_patronal = aa.id_entidad_patronal ";
-	        $whe1  = " aa.id_participes = $id_participes ";
-	        $rsConsulta1   = $busquedas->getCondicionesSinOrden($col1, $tab1, $whe1, "");
+	        $col1  = " aa.id_contribucion,aa.fecha_registro_contribucion,aa.fecha_contable_distribucion,aa.observacion_contribucion,aa.descripcion_contribucion";
+	        $tab1  = " core_contribucion aa
+	        INNER JOIN core_participes bb ON bb.id_participes = aa.id_participes
+	        INNER JOIN core_contribucion_tipo cc ON cc.id_contribucion_tipo = aa.id_contribucion_tipo ";
+	        $whe1  = " bb.id_estatus	= 1
+	        AND UPPER(cc.nombre_contribucion_tipo) = 'APORTE PERSONAL'
+	        AND extract(year from aa.fecha_registro_contribucion) = $anio_registro
+	        AND extract(month from aa.fecha_registro_contribucion) < $mes_registro
+	        AND bb.id_participes	= $id_participes ";
+	        $id1   = "aa.fecha_contable_distribucion";
+	        $rsConsulta1   = $busquedas->getCondicionesDesc($col1, $tab1, $whe1, $id1);
 	        
-	        $resp['dataParticipe'] = ( empty($rsConsulta1) ) ? null : $rsConsulta1;
-	        
-	        select aa.id_contribucion,aa.fecha_registro_contribucion,aa.fecha_contable_distribucion,aa.observacion_contribucion,aa.descripcion_contribucion, aa.*
-	        from core_contribucion aa
-	        inner join core_participes bb on bb.id_participes = aa.id_participes
-	        inner join core_contribucion_tipo cc on cc.id_contribucion_tipo = aa.id_contribucion_tipo
-	        where bb.id_estatus	= 1
-	        and upper(cc.nombre_contribucion_tipo) = 'APORTE PERSONAL'
-	        and extract(year from aa.fecha_registro_contribucion) = 2020
-	        and extract(month from aa.fecha_registro_contribucion) < 04
-	        and bb.cedula_participes	= '0912484771'
-	        order by fecha_contable_distribucion desc;
-	        
-	       	        
+	        $resp['dataRegistro'] = ( empty($rsConsulta1) ) ? null : $rsConsulta1;
+	                
 	        $error_pg = pg_last_error();
 	        if( !empty($error_pg) ){
 	            throw new Exception( $error_pg );

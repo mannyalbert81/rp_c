9895c9b004cb55b97b8601d9f1b434322d43a64b
programador1 <programador1@DANNY-DESARROLLO.CAPREMCI.INT>
2020-06-02 19:35:38 -0500

    MODULO TESORERIA ==> funcion LISTAR CUENTAR PAGAR integar plugin datatable en obetener lista de cuentas porpagar generadas

--- view/tesoreria/js/IndexCuentasPagar.js
+++ view/tesoreria/js/IndexCuentasPagar.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 	
 	
 	init();
-	load_retencion(1);
-	cargaCuentasPagar(1);
-	
+	//load_retencion(1);
+	//cargaCuentasPagar(1);
+	load_cuentas_pagar();
 		
 })
 
@@ -15,12 +15,9 @@ $(document).ready(function(){
  */
 function init(){
 	
-	
 	//mask: "9{1,10}.99",
 	$("#impuestos_cuentas_pagar").hide();
-	
-	
-	
+	iniciar_eventos_datatable();	
 }
 
 /*******************************************************************************
@@ -102,6 +99,117 @@ $("#cuentas_pagar_registrados").on('click','a.showpdf',function(event){
 
 $("#search_cuentas_pagar").on('keyup',function(){
 	cargaCuentasPagar();
+})
+
+/********************************************************** EMPIEZA PROCEOSOS CON DATATABLE *************************************************/
+
+//variable de vista
+var view	= view || {};
+view.txt_busqueda	= $("#search_cuentas_pagar");
+//variable para dataTable
+var viewTable = viewTable || {};
+
+viewTable.tabla  = null;
+viewTable.nombre = 'tbl_listado_cuentas_pagar';
+viewTable.params = { 'input_search': view.txt_busqueda.val() };
+viewTable.contenedor = $("#div_listado_cuentas_pagar");
+
+var idioma_espanol = {
+	    "sProcessing":     "Procesando...",
+        "sLengthMenu":     "Mostrar _MENU_ registros",
+        "sZeroRecords":    "No se encontraron resultados",
+        "sEmptyTable":     "Ningún dato disponible en esta tabla &#128543; ",
+        "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+        "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+        "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix":    "",
+        "sSearch":         "Buscar:",
+        "sUrl":            "",
+        "sInfoThousands":  ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst":    "Primero",
+            "sLast":     "Último",
+            "sNext":     "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        },
+        "buttons": {
+            "copy": "Copiar",
+            "colvis": "Visibilidad"
+        }
+}
+
+var load_cuentas_pagar	= function(){
+	
+	var dataSend = { 'input_search': view.txt_busqueda.val() };
+	
+	viewTable.tabla	=  $( '#'+viewTable.nombre ).DataTable({
+	    'processing': true,
+	    'serverSide': true,
+	    'serverMethod': 'post',
+	    'destroy' : true,
+	    'ajax': {
+	        'url':'index.php?controller=TesReporteCuentasPagar&action=dtListarCuentasPagar',
+	        'data': function ( d ) {
+	            return $.extend( {}, d, dataSend );
+	            },
+            'dataSrc': function ( json ) {                
+                return json.data;
+              }
+	    },	
+	    'order': [[ 1, "desc" ]],
+	    'columns': [	    	    
+	    	{ data: 'numfila', orderable: false },
+    		{ data: 'fecha'},
+    		{ data: 'cedula_proveedor' },
+    		{ data: 'nombre_proveedor'},
+    		{ data: 'numero_documento' },
+    		{ data: 'valor_documento', orderable: false },
+    		{ data: 'descripcion', orderable: false},
+    		{ data: 'opciones', orderable: false }
+	    ],
+	    'columnDefs': [
+	        {className: "dt-center", targets:[0] },
+	        {sortable: false, targets: [ 0,5,6,7 ] }
+	      ],
+		'scrollY': "80vh",
+        'scrollCollapse':true,
+        'fixedHeader': {
+            header: true,
+            footer: true
+        },
+        'language':idioma_espanol
+	 });
+		
+}
+
+var iniciar_eventos_datatable = function(){
+	
+	viewTable.contenedor.tooltip({
+	    selector: 'a.showpdf',
+	    trigger: 'hover',
+	    html: true,
+        delay: {"show": 500, "hide": 0},
+        placement:"left"
+	});
+		
+}
+
+viewTable.contenedor.on('click','a.showpdf',function(event){
+	let enlace = $(this);
+	let _url = "index.php?controller=TesCuentasPagar&action=RptCuentasPagar&id_cuentas_pagar="+enlace.data().id;
+	
+	if ( enlace.data().id ) {
+		
+		window.open(_url,"_blank");
+		
+	}
+	
+	event.preventDefault();
 })
 
 

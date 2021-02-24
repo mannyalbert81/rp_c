cd202bf4956fb7c54ed56465e3f9ac63bcec8167
Administrador <Administrador@DANNY-DESARROLLO.CAPREMCI.INT>
2021-02-24 11:18:00 -0500

    Cambios

--- core/ControladorBase.php
+++ core/ControladorBase.php
@@ -162,6 +162,17 @@ class ControladorBase{
     	require_once 'view/reportes/'.$vista.'Report.php';
     }
     
+    public function view_inversiones($vista,$datos){
+        foreach ($datos as $id_assoc => $valor) {
+            ${$id_assoc}=$valor;
+        }
+        
+        require_once 'core/AyudaVistas.php';
+        $helper=new AyudaVistas();
+        
+        require_once 'view/inversiones/'.$vista.'View.php';
+    }
+    
     public function afuera($vista,$datos){
     	foreach ($datos as $id_assoc => $valor) {
     		${$id_assoc}=$valor;

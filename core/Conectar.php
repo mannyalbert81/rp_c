187ae89f686a871248195285b0d847f21bb3e106
steve <steve@DESKTOP-80K509B>
2021-06-10 15:11:20 -0500

    fsdfds

--- core/Conectar.php
+++ core/Conectar.php
@@ -19,8 +19,8 @@ class Conectar{
         if($this->driver=="pgsql" || $this->driver==null){
             
 //steven
-            $con = pg_connect("host=192.168.1.231 port=5432 dbname=rp_capremci user=postgres password=Programadores2018");
-            // $con = pg_connect("host=186.4.157.125 port=5432 dbname=rp_capremci user=postgres password=Programadores2018");
+            //$con = pg_connect("host=192.168.1.231 port=5432 dbname=rp_capremci user=postgres password=Programadores2018");
+             $con = pg_connect("host=186.4.157.125 port=5432 dbname=rp_capremci user=postgres password=Programadores2018");
             
         	if(!$con){
         		echo "No se puedo Conectar a la Base";
@@ -42,8 +42,8 @@ class Conectar{
         	
         	try
         	{
-		        $pdo = new PDO('pgsql:host=192.168.1.231;port=5432;dbname=rp_capremci', 'postgres', 'Programadores2018' );
-        	    //$pdo = new PDO('pgsql:host=186.4.157.125;port=5432;dbname=rp_capremci', 'postgres', 'Programadores2018' );
+		        //$pdo = new PDO('pgsql:host=192.168.1.231;port=5432;dbname=rp_capremci', 'postgres', 'Programadores2018' );
+        	    $pdo = new PDO('pgsql:host=186.4.157.125;port=5432;dbname=rp_capremci', 'postgres', 'Programadores2018' );
         	    
             	$fpdo = new FluentPDO($pdo);
             	
